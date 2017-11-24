d14bf01d5628c941f2444f91edae8ec5e98062fd
vonmoraes <31371849+vonmoraes@users.noreply.github.com>
2017-11-23 22:43:45 -0200

    0.0.7
    Código funcionando completamente

--- signal.asm
+++ signal.asm
@@ -41,57 +41,57 @@ INICIO:
             MOVWF CMCON        ;COMPARADOR ANALOGICO
 ;***********************************************************************************************
 ;TESTE DOS LEDS E DO BOTAO
-;GOTO TESTE
+;GOTO TESTE7SEGUNDOS
 ;COMENTAR TESTE CASO NAO ESTEJA EM TESTE
 ;***********************************************************************************************
 LOOP
     MOVLW D'7'
     MOVWF AUX                ;NOVO VALOR AUX
     CLRF  FLAG                ;LIMPA FLAG
-FOR1
     MOVLW B'00001100'        ;S1 VERDE,S2 VERMELHO
     MOVWF PORTB
+FOR1
     CALL DELAY
     BTFSC FLAG,0
     GOTO CICLO_1            ;IR PARA O CICLO1
-    DECFSZ AUX,F
+    DECFSZ AUX,1
     GOTO FOR1                ;FIM FOR1
     MOVLW    D'3'
     MOVWF AUX                ;NOVO VALOR AO AUX
     CLRF  FLAG                ;LIMPA FLAG
-FOR2
     MOVLW    B'00001010'        ;S1 AMARELO,S2 VERMELHO
     MOVWF    PORTB
+FOR2
     CALL DELAY
     BTFSC FLAG,0
     GOTO CICLO_2			;IR PARA O CICLO2
     DECFSZ AUX,F
-    GOTO FOR2
+    GOTO FOR2				;FIM FOR2
     MOVLW D'2'
     MOVWF AUX                ;NOVO VALOR AO AUX
     CLRF  FLAG                ;LIMPA FLAG
-FOR3
     MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
     MOVWF    PORTB
+FOR3
     CALL DELAY
     BTFSC FLAG,0
     GOTO CICLO_3				;IR PARA O CICLO3
     DECFSZ AUX,F
-    GOTO FOR3
+    GOTO FOR3				;FIM FOR3
     MOVLW D'2'
     MOVWF AUX                ;NOVO VALOR AO AUX
     CLRF  FLAG                ;LIMPA FLAG
     CALL DELAY
     BTFSC FLAG,0
     GOTO CICLO_3				;IR PARA O CICLO3
-FOR4
     MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
     MOVWF    PORTB
+FOR4
     CALL DELAY
     BTFSC FLAG,0
     GOTO CICLO_3			;IR PARA O CICLO3
     DECFSZ AUX,F
-    GOTO FOR4
+    GOTO FOR4				;FIM FOR4
     GOTO LOOP
 ;***********************************************************************************************
 ;SUBROTINAS
@@ -106,104 +106,114 @@ ALLHIGH
     MOVLW B'11111111' ;ACENDE TODOS OS LEDS (RB - 0,1,2,3,4,5,X,X)
     MOVWF PORTB
 	CALL DELAY
-	CALL DELAY
-	CALL DELAY
     RETURN
 ALLLOW
     MOVLW B'00000000' ;APAGATODOS OS LEDS (RB - 0,1,2,3,4,5,X,X)
     MOVWF PORTB
     GOTO TESTE
+TESTE7SEGUNDOS
+    MOVLW B'00000000' ;APAGA TODOS OS LEDS (RB - 0,1,2,3,4,5,X,X)
+    MOVWF PORTB
+	MOVLW D'7'
+	MOVWF AUX
+FORTESTE
+	CALL DELAY
+	BTFSC FLAG,0
+	GOTO ALLHIGH
+	DECFSZ AUX,F
+	GOTO FORTESTE
+	GOTO TESTE7SEGUNDOS ; LOOP
 ;***********************************************************************************************
 ;FIM ROTINA DE TESTE E INICIO DAS ROTINAS DOS CICLOS
 ;***********************************************************************************************
 CICLO_1
-     MOVLW    D'3'
+	MOVLW    D'3'
     MOVWF    AUX
+    MOVLW    B'00001010'        ;S1 AMARELO,S2 VERMELHO
+    MOVWF    PORTB
 FORC1_2
-          MOVLW    B'00001010'        ;S1 AMARELO,S2 VERMELHO
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC1_2
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC1_2
     MOVLW    D'3'
     MOVWF    AUX
+	MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
+    MOVWF    PORTB
 FORC1_3
-          MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC1_3
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC1_3
     CALL DELAY
     MOVLW    D'3'
     MOVWF    AUX
+    MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
+    MOVWF    PORTB
 FORC1_4
-           MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC1_4
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC1_4
     MOVLW    D'4'
     MOVWF    AUX
+    MOVLW    B'00001100'        ;S1 VERDE,S2 VERMELHO
+    MOVWF    PORTB
 FORC1_1
-           MOVLW    B'00001100'        ;S1 VERDE,S2 VERMELHO
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC1_1
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC1_1
     GOTO LOOP
 CICLO_2
     MOVLW    D'3'
     MOVWF    AUX
+    MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
+    MOVWF    PORTB
 FORC2_3
-           MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC2_3
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC2_3
     MOVLW    D'3'
     MOVWF    AUX
-    NOP                            ;CORRECAO DO ATRASO
+    CALL DELAY                         ;CORRECAO DO ATRASO
+    MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
+    MOVWF    PORTB
 FORC2_4
-          MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC2_4
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC2_4
     MOVLW    D'4'
     MOVWF    AUX
+    MOVLW    B'00001100'        ;S1 VERDE,S2 VERMELHO
+    MOVWF    PORTB
 FORC2_1
-          MOVLW    B'00001100'        ;S1 VERDE,S2 VERMELHO
-            MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC2_1
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC2_1
     GOTO LOOP
 CICLO_3
     MOVLW    D'3'
     MOVWF    AUX
+    MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
+    MOVWF    PORTB
 FORC3_3
-          MOVLW    B'00100001'        ;S1 VERMELHO,S2 VERDE
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC3_3
-          CALL DELAY
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC3_3
+    CALL DELAY
     MOVLW    D'3'
     MOVWF    AUX
+    MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
+    MOVWF    PORTB
 FORC3_4
-          MOVLW    B'00010001'        ;S1 VERMELHO, S2 AMARELO
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC3_4
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC3_4
     MOVLW    D'4'
     MOVWF    AUX
+    MOVLW    B'00001100'        ;S1 VERDE,S2 VERMELHO
+    MOVWF    PORTB
 FORC3_1
-          MOVLW    B'00001100'        ;S1 VERDE,S2 VERMELHO
-          MOVWF    PORTB
-          CALL DELAY
-          DECFSZ AUX,F
-          GOTO FORC3_1
+    CALL DELAY
+    DECFSZ AUX,F
+    GOTO FORC3_1
     GOTO LOOP
 ON                              ;ALTERA VALOR DA FLAG CASO O BOTAO SEJA APERTADO
     MOVLW D'1'
